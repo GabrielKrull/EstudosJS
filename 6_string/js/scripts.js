6e59cd585d97f643aa0acb25a441b3e8358c0fe1
Gabriel Alves Krull <bielpoow@gmail.com>
2026-02-17 21:41:29 -0300

    estudo 17-02

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,24 @@
+var nome = "Gabriel";
+var sobreNome = "Alves";
+
+var nomeCompleto = nome+ " " +sobreNome
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929")
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase)
+
+document.writeln('Ele disse:"Olá"');
+
+document.writeln("Ele disse:'Olá'");
+
+console.log("Este número: " + nome);
