6612f59d3793b20c3d3fc098bf95ab7f4c5db96d
Gabriel Alves Krull <bielpoow@gmail.com>
2026-02-24 21:10:14 -0300

    estudo 24-02

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,19 @@
+var arr = [5, "Gabriel", true, {teste:1, teste:2}]; //obj => {}
+
+console.log(arr);
+
+var arr2 = [2,4,5,6,3,8];
+
+console.log(arr2);
+
+console.log(arr[2]);
+console.log(arr[0]);
+
+arr[4] = 10;
+
+arr[0] = "Teste";
+
+console.log(arr);
+
+console.log(typeof arr);
+
