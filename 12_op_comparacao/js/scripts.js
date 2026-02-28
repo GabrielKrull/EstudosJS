92712261a09eb78e069c424d4edb275165470411
Gabriel Alves Krull <bielpoow@gmail.com>
2026-02-27 22:19:55 -0300

    estudo 26-02 atrasado e 27-02

---
+++ 12_op_comparacao/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 15;
+var possuiCarro = 0;
+
+if(idade >= 18){
+    console.log("O usuário pode fazer a carteira");
+}
+
+if(idade <= 17){
+    console.log("O usuário não pode fazer a carteira");
+}
+
+if(possuiCarro){
+    console.log("O usuário pode andar de carro");
+}
+
+var nome = "Gabriel";
+
+if(nome == "Gabriel"){
+    console.log("Seu nome é Gabriel");
+}
+
+if(nome != "Gabriel"){
+    console.log("Seu nome não é Gabriel");
+}
+
+if(20 > 100){
+    console.log("Passou");
+}
+
+if(100 < 20){
+    console.log("Passou 2");
+}
---
+++ 13_else_if_e_else/js/scripts.js
@@ -0,0 +1,28 @@
+var nome = "Gabriel";
+
+if (nome == "Pedro"){
+    console.log("O nome dele é Pedro");
+}
+else if(nome == "Gabriel"){
+    console.log("O nome é Gabriel")
+}
+else if(nome == "Xavier"){
+    console.log("O nome é Xavier")
+}
+else{
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+}else if(idade <= 18){
+    console.log("Ele só pode entrar com autorização!");
+}
+
+if(nome == "Gabriel"){
+    console.log("Teste");
+}else{
+    console.log("Testando");
+}
